e4e356762d40fe95f9524976a21d8cd9b557f5ad
peter-stema <psthree@aol.com>
2019-05-27 09:49:28 -0400

    comment out old fetch code used for localhost

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,14 @@
 console.log('loaded');
 
-fetch('http://localhost:3000/weather?address=detroit').then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log('error', data.error);
-    } else {
-      console.log(data.location, data.forecast);
-    }
-  });
-});
+// fetch('http://localhost:3000/weather?address=detroit').then(response => {
+//   response.json().then(data => {
+//     if (data.error) {
+//       console.log('error', data.error);
+//     } else {
+//       console.log(data.location, data.forecast);
+//     }
+//   });
+// });
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
